9195bcbd8282c3513356e96c51f393e4416521f5
gabrielatleticanobh@gmail.com <gabrielatleticanobh@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2015-03-29 00:49:54 +0000

    Gleidson estao tentando evoluir a excecao na miha pasta de atividades entando colocar o metodo troco porem n consigo me ajude

---
+++ trunk/turma_6256/codigo-fonte/estudo/src/gabriel/excecao/MetodoTroco.java
@@ -0,0 +1,12 @@
+package gabriel.excecao;
+import gabriel.excecao.Jogo;
+
+public class MetodoTroco {
+
+	public Long trocoCompra() {
+		MetodoTroco troco = new MetodoTroco();
+		Jogo jogo;
+		Personagem personagem;
+		 = (jogo.getDinheiro()) - (personagem.getValor());
+	}
+}
