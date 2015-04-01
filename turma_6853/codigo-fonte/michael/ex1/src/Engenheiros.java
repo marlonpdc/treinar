6634ef3b8dbfa8c41a0faa10cf4ff9123d5073a3
michaeldavidsd <michaeldavidsd@gmail.com>
2015-04-01 22:01:18 +0000

    primeiro code

---
+++ turma_6853/codigo-fonte/michael/ex1/src/Engenheiros.java
@@ -0,0 +1,14 @@
+
+public class Engenheiros extends Pessoa{
+	
+	Engenheiros(){}
+	Engenheiros(String nome, String Matricula,String Cpf, String dataNascimento, String Senha, double salario)
+	{
+		super(nome,Matricula,Cpf,dataNascimento,Senha,salario);
+	}
+	
+	public void MyPrint(){
+		super.MyPrint();
+	}
+
+}
