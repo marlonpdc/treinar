3db1113dba096aa77954a07579517484150edcca
gleidson.gmoura <gleidson.gmoura@gmail.com>
2015-04-02 01:04:01 +0000

    Implementalção do Enum SituacaoConta

---
+++ turma_6853/codigo-fonte/bb/src/br/com/treinar/bb/modelo/StatusConta.java
@@ -0,0 +1,22 @@
+package br.com.treinar.bb.modelo;
+
+import java.util.List;
+
+public enum StatusConta {
+
+	ATIVA("Ativa"),
+	INATIVA("Inativa"),
+	BLOQUEADA("Bloqueada"),
+	CANCELADA("Cancelada");
+	
+	private String descricao;
+	
+	private StatusConta(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+}
