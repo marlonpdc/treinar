38c76e9e96c199a8bcbaa82369cd9bcaac4125bf
gleidson.gmoura@gmail.com <gleidson.gmoura@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2015-04-02 01:27:34 +0000

    Conhecimento Collections

---
+++ trunk/turma_6853/codigo-fonte/estudo/src/estudo/collecton/EstudoColletion.java
@@ -0,0 +1,31 @@
+package estudo.collecton;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import estudo.objeto.Pessoa;
+
+@SuppressWarnings({ "rawtypes", "unchecked" })
+public class EstudoColletion {
+
+	public static void main(String[] args) {
+		Collection c = new ArrayList<>();
+		Pessoa p = new Pessoa();
+		Pessoa p2 = new Pessoa();
+		String amanha = "Ontem";
+		
+		c.add(p);
+		c.add(p2);
+		c.add(amanha);
+		Pessoa pessoa = null;
+		for (Object object : c) {
+			if (object instanceof Pessoa) {
+				pessoa = (Pessoa)object;
+				System.out.println(pessoa.getNome());
+				System.out.println(pessoa.getAltura());
+			}
+		}
+	}
+
+	
+}
