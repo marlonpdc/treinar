71e98665909d39c21ad2a9cb4f86d8eb08b6fef3
michaeldavidsd@gmail.com <michaeldavidsd@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2015-04-08 22:27:13 +0000

    comentarios

--- trunk/turma_6853/codigo-fonte/estudo/estudo/src/estudo/io/EscreverArquivo.java
+++ trunk/turma_6853/codigo-fonte/estudo/estudo/src/estudo/io/EscreverArquivo.java
@@ -9,9 +9,9 @@ import java.io.OutputStreamWriter;
 public class EscreverArquivo {
 
 	public static void main(String[] args) throws IOException {
-		OutputStream os = new FileOutputStream("saida.txt", Boolean.TRUE);
-		OutputStreamWriter osw = new OutputStreamWriter(os);
-		BufferedWriter bw = new BufferedWriter(osw);
+		OutputStream os = new FileOutputStream("saida.txt", Boolean.TRUE); // escrever caractere em binario
+		OutputStreamWriter osw = new OutputStreamWriter(os); // escrever caractere em char
+		BufferedWriter bw = new BufferedWriter(osw); // escrever string
 		
 		bw.newLine();
 		bw.write("treinar tres");
