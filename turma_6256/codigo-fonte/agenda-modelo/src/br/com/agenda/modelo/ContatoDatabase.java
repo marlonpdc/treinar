e962e366789067695d89f7ebbf9d4e50cb37e2f9
betosilva.luz <betosilva.luz@gmail.com>
2015-04-18 19:21:56 +0000

    Criação de 1 metodo que cria 2 objetos contato

--- turma_6256/codigo-fonte/agenda-modelo/src/br/com/agenda/modelo/ContatoDatabase.java
+++ turma_6256/codigo-fonte/agenda-modelo/src/br/com/agenda/modelo/ContatoDatabase.java
@@ -1,6 +1,7 @@
 package br.com.agenda.modelo;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ContatoDatabase {
@@ -10,6 +11,7 @@ public class ContatoDatabase {
 	
 	private ContatoDatabase() {
 		contatos = new ArrayList<Contato>();
+		moc();
 	}
 	
 	static {
@@ -18,6 +20,41 @@ public class ContatoDatabase {
 	
 	public static ContatoDatabase getInstance() {
 		return instance;
+	}
+	private void moc() {
+		
+		Contato c1 = new Contato();
+		c1.setEmail("teste_01@gmail.com");
+		
+		Pessoa p1 = new Pessoa();
+		p1.setNome("Contato01");
+		p1.setDataNascimento(new Date());
+		c1.setPessoa(p1);
+		
+		Telefone t1 = new Telefone();
+		t1.setDdd(031);
+		t1.setNumero(33333);
+		t1.setTipo(TipoTelefone.CELULAR);
+		c1.setTelefone(t1);
+		
+		contatos.add(c1);
+		
+		Contato c2 = new Contato();
+		c2.setEmail("teste_02@gmail.com");
+		
+		Pessoa p2 = new Pessoa();
+		p2.setNome("Contato02");
+		p2.setDataNascimento(new Date());
+		c2.setPessoa(p2);
+		
+		Telefone t2 = new Telefone();
+		t2.setDdd(011);
+		t2.setNumero(444444);
+		t2.setTipo(TipoTelefone.RESIDENCIAL);
+		c2.setTelefone(t2);
+		contatos.add(c2);
+		
+	
 	}
 
 	public List<Contato> getContatos() {
