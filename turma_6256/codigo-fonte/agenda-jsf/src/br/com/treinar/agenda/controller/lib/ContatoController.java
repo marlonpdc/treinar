c193c0b73a8fe1de07813d1eb23bf959c11cc294
betosilva.luz <betosilva.luz@gmail.com>
2015-05-02 16:50:00 +0000

    Criarção do metodo "novo()";

--- turma_6256/codigo-fonte/agenda-jsf/src/br/com/treinar/agenda/controller/lib/ContatoController.java
+++ turma_6256/codigo-fonte/agenda-jsf/src/br/com/treinar/agenda/controller/lib/ContatoController.java
@@ -11,6 +11,7 @@ import javax.faces.context.FacesContext;
 import br.com.agenda.modelo.Contato;
 import br.com.agenda.modelo.Pessoa;
 import br.com.agenda.modelo.Telefone;
+import br.com.treinar.agenda.modelo.Acao;
 
 @ManagedBean
 @SessionScoped
@@ -18,18 +19,23 @@ public class ContatoController {
 
 	private Contato contato;
 	private List<Contato> contatos;
-	
+	private Acao acao;
+
 	public ContatoController() {
 		contatos = new ArrayList<Contato>();
 		criarNovoContato();
 	}
-	
+
+	public void novo() {
+		acao = Acao.CADASTRAR;
+	}
+
 	private void criarNovoContato() {
 		contato = new Contato();
 		contato.setPessoa(new Pessoa());
 		contato.setTelefone(new Telefone());
 	}
-	
+
 	public String salvar() {
 		contatos.add(contato);
 		criarNovoContato();
@@ -37,26 +43,38 @@ public class ContatoController {
 		FacesContext.getCurrentInstance().addMessage(null, message);
 		return "listacontato.xhtml";
 	}
-	
-	public void editar(Contato contato) {
-		System.out.println(contato);
+
+	public Acao getAcao() {
+		return acao;
 	}
-	
+
+	public void setAcao(Acao acao) {
+		this.acao = acao;
+	}
+
+	public void editar(Contato contato) {
+		this.contato = contato;
+		acao = Acao.EDITAR;
+	}
+
 	public void excluir(Contato contato) {
 		System.out.println(contato);
 	}
-	
+
 	public Contato getContato() {
 		return contato;
 	}
+
 	public void setContato(Contato contato) {
 		this.contato = contato;
 	}
+
 	public List<Contato> getContatos() {
 		return contatos;
 	}
+
 	public void setContatos(List<Contato> contatos) {
 		this.contatos = contatos;
 	}
-	
+
 }
