ff63f1fb380c75988ec4ee29df17ff61d224277c
stephaniebozi@gmail.com <stephaniebozi@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2015-07-03 23:38:38 +0000

    n

---
+++ trunk/turma_7173/stephanie/estudo-java/src/Coisas/Minha/Estudo/Carro.java
@@ -0,0 +1,15 @@
+package Coisas.Minha.Estudo;
+
+public class Carro {
+	String cor;
+	int velocidade;
+	String modelo;
+
+	void acelerar() {
+		velocidade++;
+	}
+
+	void desacelerar() {
+		velocidade--;
+	}
+}
---
+++ trunk/turma_7173/stephanie/estudo-java/src/Coisas/Minha/Estudo/ComandoCondicional.java
@@ -0,0 +1,21 @@
+package Coisas.Minha.Estudo;
+
+public class ComandoCondicional {
+	
+	public static void main(String[] args) {
+		int a = 10;
+		boolean b = true;
+
+		if ( a== 10 && b) {
+			System.out.println("a == 10 && b : Verdadeiro");
+		} else {
+			System.out.println("a== 10 && b: Falso");
+}
+		
+		if (!b) {
+			System.out.println("b : false");
+}
+			System.out.println("a== 10 && b: Falso");
+	}
+
+}
