cbfe1e9dfb4152c55271467ae4987beba12fcd73
jheffbhz@gmail.com <jheffbhz@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2015-07-04 01:37:49 +0000

    Toma

---
+++ trunk/turma_7173/jeferson/estudo-java/src/br/com/pandora/atividade/Cliente.java
@@ -0,0 +1,13 @@
+package br.com.pandora.atividade;
+
+public class Cliente {
+	
+	String nome;
+	String sobrenome;
+	String telefone;
+	Endereco Endereco;
+	int idade;
+	int cpf;
+	int rg;
+	
+}
---
+++ trunk/turma_7173/jeferson/estudo-java/src/br/com/pandora/atividade/Endereco.java
@@ -0,0 +1,10 @@
+package br.com.pandora.atividade;
+
+public class Endereco {
+	
+	String rua;
+	int numero;
+	String complemento;
+	String tipoCasa;
+
+}
