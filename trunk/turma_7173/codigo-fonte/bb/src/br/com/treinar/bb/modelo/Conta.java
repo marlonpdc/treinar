940a019ad2e5a06006e0c826d00f19bf920095c7
warleymp@gmail.com <warleymp@gmail.com@b2e5ba00-9525-881a-c0fc-fff420c4917b>
2015-07-07 00:38:08 +0000

    Implementação do método sacar respeitando a regra de negócio onde o saldo não pode ser maior que o valor solicitado.

--- trunk/turma_7173/codigo-fonte/bb/src/br/com/treinar/bb/modelo/Conta.java
+++ trunk/turma_7173/codigo-fonte/bb/src/br/com/treinar/bb/modelo/Conta.java
@@ -4,9 +4,16 @@ public class Conta {
 
 	double saldo;
 	Cliente cliente;
-	
+
 	public boolean sacar(double valor) {
-		
+		if (saldo >= valor) {
+			saldo -= valor;
+			return true;
+		} else {
+			return false;
+
+		}
+
 	}
-	
+
 }
