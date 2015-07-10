133145fba4c691e51d2be1b3fc5123999dab4330
gleidson.gmoura <gleidson.gmoura@gmail.com>
2015-07-10 01:26:14 +0000

    equals and heshCode

---
+++ turma_7173/gleidson/estudo-java/src/br/com/treinar/wrapper/TesteWrapper.java
@@ -0,0 +1,39 @@
+package br.com.treinar.wrapper;
+
+import java.io.PrintStream;
+
+public class TesteWrapper {
+
+	public static void main(String[] args) {
+		
+		Integer i = Integer.valueOf(1000);
+		Integer j = 1000;
+		
+		int k = 1000;
+		int l = 1000;
+		
+		System.out.println(i == j);
+		System.out.println(k == l);
+		
+		String umStr = "1" ;
+		
+		Integer umInteger = Integer.valueOf(umStr);
+		//Integer umInteger = Integer.parseInt(umStr);
+		
+		System.out.println(umInteger + 1);
+		
+		Long numLong = 10l;
+		System.out.println(numLong);
+		
+		Boolean boo = Boolean.FALSE;
+		System.out.println(boo);
+		
+		Character c = 'a';
+		
+		
+		
+		
+		
+	}
+	
+}
